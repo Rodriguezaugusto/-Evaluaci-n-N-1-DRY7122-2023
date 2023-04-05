23c7f37e2ff1e0516790debf11dc1e339eca0813
Rodriguezaugusto <129934695+Rodriguezaugusto@users.noreply.github.com>
2023-04-05 03:07:56 -0400

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,10 @@
+nombre = input("Ingresa tu nombre: ")
+apellido = input("Ingresa tu apellido: ")
+codigo_seccion = input("Ingresa tu código de sección: ")
+sede = input("Ingresa tu sede: ")
+
+print("La información ingresada es la siguiente:")
+print("Nombre: " + nombre)
+print("Apellido: " + apellido)
+print("Código de sección: " + codigo_seccion)
+print("Sede: " + sede)
